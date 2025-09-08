f928d84572c7c92686494c04fd921ad4d70f4c59
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2025-09-09 00:56:43 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1013,5 +1013,6 @@ def rename_chat():
     return jsonify({"title": title})
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  
+    app.run(host='0.0.0.0', port=port, debug=True)
